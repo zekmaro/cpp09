1c357d1141c6a177ac5b543dd1a822d3263386eb
Andrey Arama <anarama@c3r4p6.42vienna.com>
2024-10-24 18:53:14 +0200

    starting ex00

--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -6,7 +6,27 @@
 /*   By: anarama <anarama@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 12:22:04 by anarama           #+#    #+#             */
-/*   Updated: 2024/10/24 12:22:05 by anarama          ###   ########.fr       */
+/*   Updated: 2024/10/24 14:14:50 by anarama          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef BITCOIN_EXCHANGE_HPP
+# define BITCOIN_EXCHANGE_HPP
+
+# include <string>
+# include <map>
+
+class BitcoinExchange {
+	private:
+		std::map<std::string, int> _inputData;
+
+	public:
+		BitcoinExchange( void );
+		BitcoinExchange( const BitcoinExchange& other );
+		BitcoinExchange& operator=( const BitcoinExchange& other );
+		~BitcoinExchange( void );
+
+		
+};
+
+# endif // BITCOIN_EXCHANGE_HPP
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,14 +6,24 @@
 /*   By: anarama <anarama@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 12:22:36 by anarama           #+#    #+#             */
-/*   Updated: 2024/10/24 12:24:53 by anarama          ###   ########.fr       */
+/*   Updated: 2024/10/24 13:57:36 by anarama          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <fstream>
+#include <iostream>
 #include "BitcoinExchange.hpp"
 
 int main(int argc, char **argv) {
-	
+	if (argc != 2) {
+		std::cout << "Error: could not open file." << std::endl;
+		return 1;
+	}
+	std::ifstream intputFile(argv[1]);
+	if (!intputFile.is_open()) {
+		std::cout << "Error: could not open file." << std::endl;
+		return 1;
+	}
 	
 	return 0;
 }
